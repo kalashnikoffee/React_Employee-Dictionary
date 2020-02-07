8d7e9eb8cc1010e6f5adfb6dcf6baa6e6b14d8b3
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-02-07 00:57:24 -0800

    main js components assembly

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,29 +1,13 @@
-import React from "react";
-import SearchBar from "./SearchBar.js";
-import "../styles/NavBar.css";
+import React, { Component } from "react";
+import InfoMain from "./DataArea";
+// import "../styles/Main.css";
 
-function Nav({ handleSearchChange }) {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <button
-        className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse row" id="navbarNav">
-        <div className="input-group col-8">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="">
-              DOB
-            </span>
-          </div>
-          <input type="date" className="form-control" />
-          <input type="date" className="form-control" />
-        </div>
-        <div className="search-area col-4">
-          <SearchBar handleSearchChange={handleSearchChange} />
-        </div>
-      </div>
-    </nav>
-  );
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+        <InfoMain />
+      </>
+    );
+  }
 }
-export default Nav;
